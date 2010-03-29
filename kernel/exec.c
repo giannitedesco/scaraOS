496ffc7caf79feacf4c41ce42d2175267d7f1bfd
Gianni Tedesco <gianni@scaramanga.co.uk>
2010-03-29 17:42:08 +0100

    Oops, include kernel/exec.c, not that it does much
    But it was included in the makefile in the previous commit...

---
+++ kernel/exec.c
@@ -0,0 +1,11 @@
+#include <kernel.h>
+#include <arch/idt.h>
+#include <arch/regs.h>
+#include <arch/syscalls.h>
+#include <task.h>
+
+uint32_t syscall_exec(uint32_t path)
+{
+	printk("syscall: EXEC\n");
+	return -1;
+}
