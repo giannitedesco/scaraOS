841d0658b2201dad8879a2c7194908598f0e4a17
Gianni Tedesco <gianni@scaramanga.co.uk>
2010-10-10 06:28:45 +0100

    Fix include guard for serio.h

--- include/arch-ia32/serio.h
+++ include/arch-ia32/serio.h
@@ -1,9 +1,9 @@
-#ifndef __ARCH_IA32_VGA__
-#define __ARCH_IA32_VGA__
+#ifndef __ARCH_IA32_SERIO__
+#define __ARCH_IA32_SERIO__
 
 #include <scaraOS/mm.h>
 
 void serio_init(void);
 void serio_put(uint8_t c);
 
-#endif /* __ARCH_IA32_VGA__ */
+#endif /* __ARCH_IA32_SERIO__ */
