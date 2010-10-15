3b22cd865f9bfc304a7e00d85a5d3365a0b35e3e
Tom Barlow <tomwbarlow@gmail.com>
2010-10-15 19:17:58 +0100

    Cleaned up code, we now have a simple version which can detect the drives without all the crazy bitwise operations that I have no idea what are for.

--- arch-ia32/ide.c
+++ arch-ia32/ide.c
@@ -6,80 +6,52 @@
 */
 #include <scaraOS/kernel.h>
 #include <arch/io.h>
-#include <arch/ata.h>
+#include <arch/ide.h>
 
-struct IDEChannelRegisters {
-   unsigned short base;  // I/O Base.
-   unsigned short ctrl;  // Control Base
-   unsigned short bmide; // Bus Master IDE
-   unsigned char  nIEN;  // nIEN (No Interrupt);
-} channels[2];
+struct ide_channel {
+   uint16_t base,  // I/O Base.
+			ctrl,  // Control Base
+			bmide; // Bus Master IDE
+   uint8_t  nIEN;  // nIEN (No Interrupt);
+}channels[]={
+	{ATA_BAR0,ATA_BAR1,ATA_BAR4+0,0},	// Using default BAR locs. for PATA
+	{ATA_BAR2,ATA_BAR3,ATA_BAR4+8,0}
+};
 
-static void ide_write(uint8_t channel, uint8_t reg, uint8_t data) {
-   if (reg > 0x07 && reg < 0x0C)
-      ide_write(channel, ATA_REG_CONTROL, 0x80 | channels[channel].nIEN);
-   if (reg < 0x08)
-      outb(channels[channel].base  + reg - 0x00, data);
-   else if (reg < 0x0C)
-      outb(channels[channel].base  + reg - 0x06, data);
-   else if (reg < 0x0E)
-      outb(channels[channel].ctrl  + reg - 0x0A, data);
-   else if (reg < 0x16)
-      outb(channels[channel].bmide + reg - 0x0E, data);
-   if (reg > 0x07 && reg < 0x0C)
-      ide_write(channel, ATA_REG_CONTROL, channels[channel].nIEN);
+
+static void ide_write(struct ide_channel channel, uint8_t reg_offset, 
+uint8_t data) {
+	outb(channel.base  + reg_offset, data);
 }
 
-static uint8_t ide_read(uint8_t channel, uint8_t reg) {
-   uint8_t result;
-   if (reg > 0x07 && reg < 0x0C)
-      ide_write(channel, ATA_REG_CONTROL, 0x80 | channels[channel].nIEN);
-   if (reg < 0x08)
-      result = inb(channels[channel].base + reg - 0x00);
-   else if (reg < 0x0C)
-      result = inb(channels[channel].base  + reg - 0x06);
-   else if (reg < 0x0E)
-      result = inb(channels[channel].ctrl  + reg - 0x0A);
-   else if (reg < 0x16)
-      result = inb(channels[channel].bmide + reg - 0x0E);
-   if (reg > 0x07 && reg < 0x0C)
-      ide_write(channel, ATA_REG_CONTROL, channels[channel].nIEN);
-   return result;
+static uint8_t ide_read(struct ide_channel channel, uint8_t reg_offset) {
+	uint8_t result;
+	result = inb(channel.base  + reg_offset);
+	return result;
 }
 
 static void __init ata_init(void)
 {
-	int i, j;
+	int i,j;
 	
-	channels[ATA_PRIMARY].base = (ATA_BAR0 & 0xFFFFFFFC) + 0x1F0 * (!ATA_BAR0);
-	channels[ATA_PRIMARY].ctrl = (ATA_BAR1 & 0xFFFFFFFC) + 0x3F4 * (!ATA_BAR1);
-	channels[ATA_SECONDARY].base = (ATA_BAR2 & 0xFFFFFFFC) + 0x170 * (!ATA_BAR2);
-	channels[ATA_SECONDARY].ctrl = (ATA_BAR3 & 0xFFFFFFFC) + 0x374 * (!ATA_BAR3);
-	channels[ATA_PRIMARY].bmide = (ATA_BAR4 & 0xFFFFFFFC) + 0;
-	channels[ATA_SECONDARY].bmide = (ATA_BAR4 & 0xFFFFFFFC) + 8;
+	ide_write(channels[ATA_PRIMARY], ATA_REG_CONTROL, 2);
+	ide_write(channels[ATA_SECONDARY], ATA_REG_CONTROL, 2);
 	
-   ide_write(ATA_PRIMARY  , ATA_REG_CONTROL, 2);
-   ide_write(ATA_SECONDARY, ATA_REG_CONTROL, 2);
-
-	for (i = 0; i < 2; i++) {
-		for (j = 0; j < 2; j++) {
-			// (I) Select Drive:
-         ide_write(i, ATA_REG_HDDEVSEL, 0xA0 | (j << 4)); // Select Drive.
-
-         // (II) Send ATA Identify Command:
-         ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY);
-
-         // (III) Polling:
-         	if (ide_read(i, ATA_REG_STATUS) == 0) {
-				printk("ATA: No device at Channel: %i, Drive: %i\n", i, j);
-				continue;
-			}  else {
-				printk("ATA: Device found at Channel: %i, Drive: %i\n", i, j);
-			} // If Status = 0, No Device.
+	for(i=0;i<2;i++) {
+		for(j=0;j<2;j++) {
+			// 1: Select drive command sent, not sure what 0xA0 is.
+			ide_write(channels[i], ATA_REG_HDDEVSEL, 0xA0 | (j << 4));
+			
+			// 2: Identify drive command
+			ide_write(channels[i], ATA_REG_COMMAND, ATA_CMD_IDENTIFY);
+			
+			// 3: See what drives we have active
+			if(ide_read(channels[i], ATA_REG_STATUS) != 0) {
+				printk("IDE: Drive detected at %i,%i\n", i, j);
+			}
+			
 		}
 	}
-		
-	
 }
 
 driver_init(ata_init);
