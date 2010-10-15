6a1f4785ed714b30c15960454773361733dab62d
Gianni Tedesco <gianni@scaramanga.co.uk>
2010-10-16 00:08:33 +0100

    pci: tidy up hard-coded port addresses

--- arch-ia32/pci.c
+++ arch-ia32/pci.c
@@ -2,7 +2,7 @@
 #include <arch/io.h>
 #include <arch/pci.h>
 
-#define PCICMD(bus, dev, fn)	(0x80000000 | (bus << 16) | (dev << 11) | (fn << 3))
+#define PCICMD(bus, dev, fn)	(0x80000000 | (PCI_BDF(bus, dev, fn) << 3))
 
 static unsigned int pci_conf;
 
@@ -10,26 +10,23 @@ static int __init pci_probe(void)
 {
 	uint32_t tmp;
 
-	outb(0x0cfb, 0x01);
+	outb(PCI_CONF_MODE, 0x01);
 	tmp = inl(PCI_CONFREG);
 	outl(PCI_CONFREG, 0x80000000);
 	if (inl(PCI_CONFREG) == 0x80000000) {
 		outl(PCI_CONFREG, tmp);
-		printk("pci: Using configuration 1\n");
 		return 1;
 	}
 	outl(PCI_CONFREG, tmp);
 
-	outb(0x0cfb, 0x00);
+	outb(PCI_CONF_MODE, 0x00);
 	outb(PCI_CONFREG, 0x00);
 	outb(0x0cfa, 0x00);
 	if ( (inb(PCI_CONFREG) == 0x00) && 
 		(inb(0xCFA) == 0x00)) {
-		printk("pci: Using configuration 2 (deprecated)\n");
 		return 2;
 	}
 
-	printk("pci: no pci busses detected\n");
 	return 0;
 }
 
@@ -37,23 +34,22 @@ static int __init pci_probe(void)
 static void __init pci_detect_dev(int b, int d, int f)
 {
 	long flags;
-
-	uint32_t id,class;
-	uint32_t loc=PCICMD(b,d,f);
+	uint32_t id, class;
+	uint32_t loc = PCICMD(b,d,f);
 
 	lock_irq(flags);
-	outl(PCI_CONFREG, loc | (PCI_CONF_ID<<2));
-	id=inl(PCI_CONFDATA);
-	outl(PCI_CONFREG, loc | (PCI_CONF_CLASS<<2));
-	class=inl(PCI_CONFDATA);
+	outl(PCI_CONFREG, loc | (PCI_CONF_ID << 2));
+	id = inl(PCI_CONFDATA);
+	outl(PCI_CONFREG, loc | (PCI_CONF_CLASS << 2));
+	class = inl(PCI_CONFDATA);
 	unlock_irq(flags);
 
-	if ( id!=0xffffffff ) {
+	if ( id != 0xffffffff ) {
 		printk("pci-dev: %i:%i.%i vendor=%.4lx "
 			"device=%.4lx class=%.2lx.%.2lx.%.2lx\n",
 			b, d, f,
 			id & 0xffff, id >> 16,
-			class>>24, class>>16&0xff, class>>8&0xff);
+			class >> 24, class >> 16 & 0xff, class >> 8 & 0xff);
 	}
 }
 
@@ -74,8 +70,12 @@ void __init pci_init(void)
 	unlock_irq(flags);
 
 	/* We can only deal with conf1 for now */
-	if ( pci_conf != 1 ) {
-		printk("pci: PCI access method not supported, or not found\n");
+	switch(pci_conf) {
+	case 1:
+		dprintk("pci: Using configuration 1\n");
+		break;
+	default:
+		printk("pci: Using configuration %d (deprecated)\n", pci_conf);
 		return;
 	}
 
--- include/arch-ia32/pci.h
+++ include/arch-ia32/pci.h
@@ -2,6 +2,7 @@
 #define __ARCH_IA32_PCI__
 
 #define PCI_CONFREG		0x0cf8
+#define PCI_CONF_MODE		0x0cfb
 #define PCI_CONFDATA		0x0cfc
 
 #define PCI_CONF_ID		0x00	/* vendor and device ID, 16 bits each */
@@ -9,6 +10,14 @@
 #define	PCI_CONF_CLASS		0x02	/* class & revision, class is top 16 bits */
 #define PCI_CONF_SUNDRIES	0x03	/* cache, latency, header & bist; 8 bits each */
 #define PCI_CONF_IOADDR		0x04	/* I/O base address in top 27 bits */
+
+#define PCI_FUNC_SHIFT		0
+#define PCI_DEV_SHIFT		8
+#define PCI_BUS_SHIFT		11
+
+#define PCI_BDF(bus, dev, fn) 	(bus << PCI_BUS_SHIFT) | \
+					(dev << PCI_DEV_SHIFT) | \
+					(fn << PCI_FUNC_SHIFT)
 
 #ifndef __ASM__
 void __init pci_init(void);
