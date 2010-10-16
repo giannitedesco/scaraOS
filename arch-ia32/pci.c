95cb2bfa53db551eea72c4aeba103a877c1d9b14
Gianni Tedesco <gianni@scaramanga.co.uk>
2010-10-16 01:03:40 +0100

    pci: print out device BAR's

--- arch-ia32/pci.c
+++ arch-ia32/pci.c
@@ -33,24 +33,66 @@ static int __init pci_probe(void)
 /* TODO: Grab whole configuration space */
 static void __init pci_detect_dev(int b, int d, int f)
 {
-	long flags;
 	uint32_t id, class;
 	uint32_t loc = PCICMD(b,d,f);
+	unsigned int i;
+	long flags;
 
 	lock_irq(flags);
+
 	outl(PCI_CONFREG, loc | (PCI_CONF_ID << 2));
 	id = inl(PCI_CONFDATA);
 	outl(PCI_CONFREG, loc | (PCI_CONF_CLASS << 2));
 	class = inl(PCI_CONFDATA);
-	unlock_irq(flags);
 
-	if ( id != 0xffffffff ) {
-		printk("pci-dev: %i:%i.%i vendor=%.4lx "
-			"device=%.4lx class=%.2lx.%.2lx.%.2lx\n",
-			b, d, f,
-			id & 0xffff, id >> 16,
-			class >> 24, class >> 16 & 0xff, class >> 8 & 0xff);
+	if ( id == 0xffffffff )
+		goto out;
+
+	printk("pci-dev: %i:%i.%i vendor=%.4lx "
+		"device=%.4lx rev=0x%.2x class=%.2lx.%.2lx.%.2lx\n",
+		b, d, f,
+		id & 0xffff, id >> 16,
+		class & 0xff,
+		class >> 24, class >> 16 & 0xff, class >> 8 & 0xff);
+	
+	for(i = 0; i < PCI_NUM_BARS; i++) {
+		uint32_t bar, sz, io;
+		outl(PCI_CONFREG, loc | ((PCI_CONF_BAR0 + i) << 2));
+		bar = inl(PCI_CONFDATA);
+		outl(PCI_CONFDATA, ~0UL);
+		sz = inl(PCI_CONFDATA);
+		if ( !bar )
+			continue;
+		if ( bar & 1 ) {
+			io = 1;
+			bar &= ~0x3;
+			sz = ~(sz & ~0x3) + 1;
+		}else{
+			uint32_t mask;
+			switch ( (bar & 0x3) >> 1 ) {
+			case 0:
+				mask = ~0xfUL;
+				break;
+			case 1:
+				mask = ~0xfUL;
+				break;
+			case 2:
+				i++;
+				continue;
+			default:
+				/* ??? */
+				continue;
+			}
+			io = 0;
+			bar &= mask;
+			sz = ~(sz & mask) + 1;
+		}
+		printk(" - %s BAR%d (%8u @ 0x%.8x)\n",
+			(io) ? "I/O" : "MEM", i, sz, bar);
 	}
+
+out:
+	unlock_irq(flags);
 }
 
 static void __init pci_scan_bus(int bus)
--- include/arch-ia32/pci.h
+++ include/arch-ia32/pci.h
@@ -5,11 +5,20 @@
 #define PCI_CONF_MODE		0x0cfb
 #define PCI_CONFDATA		0x0cfc
 
-#define PCI_CONF_ID		0x00	/* vendor and device ID, 16 bits each */
-#define PCI_CONF_CMDSTAT	0x01	/* commands & stats, 16 bits each */
-#define	PCI_CONF_CLASS		0x02	/* class & revision, class is top 16 bits */
-#define PCI_CONF_SUNDRIES	0x03	/* cache, latency, header & bist; 8 bits each */
-#define PCI_CONF_IOADDR		0x04	/* I/O base address in top 27 bits */
+#define PCI_NUM_BARS		6
+
+#define PCI_CONF_ID		0x00 /* vendor and device ID */
+#define PCI_CONF_CMDSTAT	0x01 /* commands & stats */
+#define	PCI_CONF_CLASS		0x02 /* class & revision */
+#define PCI_CONF_BIST		0x03 /* bist, type, timer, cache line size */
+#define PCI_CONF_BAR0		0x04
+#define PCI_CONF_BAR1		0x05
+#define PCI_CONF_BAR2		0x06
+#define PCI_CONF_BAR3		0x07
+#define PCI_CONF_BAR4		0x08
+#define PCI_CONF_BAR5		0x09
+#define PCI_CONF_SUNDRIES	0x03 /* cache, latency, header & bist; 8 bits each */
+#define PCI_CONF_IOADDR		0x04 /* I/O base address in top 27 bits */
 
 #define PCI_FUNC_SHIFT		0
 #define PCI_DEV_SHIFT		8
