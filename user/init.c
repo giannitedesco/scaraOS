a6d00a302cce4b1e348baaebb70d79fe9236a10a
Gianni Tedesco <gianni@scaramanga.co.uk>
2010-11-06 05:49:09 +0000

    init: 0xdeadbeef triggers uaccess check not a proper pagefault
    Let's try and write from 0xbeefdead as well...

--- user/init.c
+++ user/init.c
@@ -7,7 +7,9 @@ int main(int argc, char **argv)
 
 	if ( _write(STDOUT_FILENO, hello_world, strlen(hello_world)) <= 0 )
 		return EXIT_FAILURE;
+
 	_write(STDOUT_FILENO, (void *)0xdeadbeef, 20);
+	_write(STDOUT_FILENO, (void *)0xbeefdead, 20);
 
 	switch (_fork(FORK_PID|FORK_FD|FORK_FS|FORK_MEM, NULL, NULL, NULL)) {
 	case -1:
